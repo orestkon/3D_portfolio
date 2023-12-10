f7634ccf257c7f8dab39d42374c94fcb57ec7584
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2023-12-10 20:44:22 +0200

    Folder structure rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import { Home, About, Projects, Contact } from './pages';
 
 const App = () => {
   return (
